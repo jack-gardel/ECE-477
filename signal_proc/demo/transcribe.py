d70a0f74c9df56dfd900e06ae25e9f13ab0bbf3e
jack-gardel <jgardel@purdue.edu>
2023-04-03 15:31:34 -0400

    transcribe works with picovoice

--- signal_proc/demo/transcribe.py
+++ signal_proc/demo/transcribe.py
@@ -7,14 +7,16 @@ F_sample = 16000
 
 def get_audio_data():
   print("RECORDING...")
-  waveform = sd.rec(T_recording*F_sample, samplerate=F_sample, channels=1, dtype='int64')
+  waveform = sd.rec(T_recording*F_sample, samplerate=F_sample, channels=1, dtype='int16')
   sd.wait()
   print("DONE")
-  return waveform
+  #pdb.set_trace()
+  return waveform.reshape((-1,))
 
 leopard = pv.create(access_key="4aaEbdDymSW3u6gRGfDwdsH6WYmaa7GcONy0AfQibvpJTMyfRlVV4A==")
 
-transcript, words = leopard.process_file("harvard.wav")
+#transcript, words = leopard.process_file("harvard.wav")
+transcript, words = leopard.process(get_audio_data())
 print(transcript)
 for word in words:
   print("{word=\"%s\" start_sec=%.2f end_sec=%.2f confidence=%.2f}" % (word.word, word.start_sec, word.end_sec, word.confidence))
